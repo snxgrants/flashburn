08aab193958deac9b1ef018a15c7db46989fe7e3
Gautham Ganesh Elango <gautham.gg@gmail.com>
2021-07-29 16:12:55 +1000

    update lqyout

--- packages/interface/src/components/layout.tsx
+++ packages/interface/src/components/layout.tsx
@@ -34,7 +34,7 @@ function Layout({ children }: { children: ReactNode }): JSX.Element {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="manifest" href="/manifest.json" />
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="msapplication-TileColor" content="#00d1ff" />
         <meta name="theme-color" content="#ffffff" />
         <meta property="og:type" content="website" />
         <meta property="og:image" content={siteLogo} />
