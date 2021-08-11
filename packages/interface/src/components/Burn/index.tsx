456dcf695bcb5f449d83f537780a5607c04736d1
Gautham Ganesh Elango <gautham.gg@gmail.com>
2021-08-11 12:58:21 +1000

    slippage

--- packages/interface/src/components/Burn/index.tsx
+++ packages/interface/src/components/Burn/index.tsx
@@ -68,7 +68,7 @@ function Burn({
   } = balances;
   const isRefreshDisabled: boolean = loading || sUSDAmountBN.toString() === "0";
 
-  const options: string[] = ["0.1", "0.5", "1", "3"];
+  const slippageOptions: string[] = ["0.1", "0.5", "1", "3"];
 
   return (
     <Box {...props}>
@@ -129,18 +129,28 @@ function Burn({
             <PopoverCloseButton marginTop="1" />
             <PopoverHeader>Settings</PopoverHeader>
             <PopoverBody>
-              <Flex justifyContent="space-between">
-                <Text marginTop="1">Slippage</Text>
+              <Text marginBottom="2">Slippage tolerance</Text>
+              <Flex>
                 <Select
-                  width="25"
+                  width="28"
                   height="8"
+                  marginRight="1"
                   value={slippage}
                   onChange={(e) => setSlippage(e.target.value)}
                 >
-                  {options.map((value) => (
+                  {slippageOptions.map((value: string) => (
                     <option key={value} value={value}>{`${value}%`}</option>
                   ))}
                 </Select>
+                <Button
+                  bg="#06061B"
+                  border="1px"
+                  borderColor="white"
+                  size="sm"
+                  onClick={() => setSlippage("0.5")}
+                >
+                  Auto
+                </Button>
               </Flex>
             </PopoverBody>
           </PopoverContent>
